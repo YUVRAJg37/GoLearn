2e6d24f550ad1e179752d83cc231e850fd9a69f5
Yuvi <yuvi@192.168.1.80>
2025-08-09 00:47:56 +0530

    Update | Using Json & Upate functionality

--- todo/main.go
+++ todo/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,38 +10,28 @@ import (
 )
 
 type TodoItem struct {
-	id          int
-	isDone      bool
-	description string
+	Id          int    `json:"id"`
+	IsDone      bool   `json:"isDone"`
+	Description string `json:"description"`
 }
+
+type mode int
+
+const (
+	DELETE mode = iota
+	UPDATE
+)
+
+const FILE_NAME = "data.json"
 
 var reader = bufio.NewReader(os.Stdin)
 
 func main() {
-
-	file, err := os.OpenFile("data.csv", os.O_APPEND|os.O_CREATE, os.ModeAppend)
-
-	if err != nil {
-		fmt.Println("Error Reading CSV: ", err)
-		file.Close()
-	}
-
-	defer file.Close()
-
-	records, err := csv.NewReader(file).ReadAll()
-
-	if err != nil {
-		fmt.Println("Error Reading CSV: ", err)
-		file.Close()
-	}
-
-	writer := csv.NewWriter(file)
-
-	var index int = len(records)
-
 	for {
 		println("Choose the Options.")
 		println("1. Add Item")
+		println("2. Remove Item")
+		println("3. Mark Item")
 		println("(Q/q) quit.")
 		userInput, err := takeInputFromUser("")
 
@@ -55,26 +45,69 @@ func main() {
 			break
 		}
 
-		if userInput == "1" {
+		switch userInput {
+		//-----Creation-----
+		case "1":
 			input, err := takeInputFromUser("Enter your task: ")
 			if err != nil {
-				fmt.Println("Error reading task: ", err)
-				break
+				fmt.Println(err)
+				return
 			}
 
-			index = index + 1
-			data := []string{strconv.Itoa(index), "false", input}
+			if strings.TrimSpace(input) == "" {
+				fmt.Println("Input not valid")
+				return
+			}
 
-			err = writer.Write(data)
+			err = handleItemAddition(input)
 
 			if err != nil {
-				fmt.Println("Error Writing CSV: ", err)
-				file.Close()
-				writer.Flush()
-				break
+				fmt.Println(err)
+				return
+			}
+		//-----Deletion-----
+		case "2":
+			input, err := takeInputFromUser("Enter item id to remove: ")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			id, err := strconv.Atoi(input)
+
+			if err != nil {
+				fmt.Println(err)
+				return
 			}
 
-			fmt.Println("Task Added: ", input)
+			err = handleUpdation(id, DELETE)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		//-----Mark-----
+		case "3":
+			input, err := takeInputFromUser("Enter item id to mark: ")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			id, err := strconv.Atoi(input)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			err = handleUpdation(id, UPDATE)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		default:
+			fmt.Println("Invalid Input")
+			continue
 		}
 
 		userInput, err = takeInputFromUser("Continue....?[(Y/y)/(N/n)]")
@@ -91,12 +124,6 @@ func main() {
 		}
 
 	}
-
-	writer.Flush()
-	err = writer.Error()
-	if err != nil {
-		fmt.Println(err)
-	}
 }
 
 func takeInputFromUser(prompt string) (string, error) {
@@ -105,4 +132,65 @@ func takeInputFromUser(prompt string) (string, error) {
 	}
 	input, err := reader.ReadString('\n')
 	return strings.TrimSpace(input), err
+}
+
+func handleItemAddition(description string) error {
+	jsonData, err := ReadJson[TodoItem](FILE_NAME)
+
+	if err != nil {
+		return err
+	}
+
+	data := TodoItem{
+		Id:          len(jsonData),
+		IsDone:      false,
+		Description: description,
+	}
+
+	jsonData = append(jsonData, data)
+	fmt.Println(jsonData)
+	return WriteJson(string(FILE_NAME), jsonData)
+}
+
+func handleUpdation(id int, m mode) error {
+	jsonData, err := ReadJson[TodoItem](FILE_NAME)
+
+	if err != nil {
+		return err
+	}
+
+	if len(jsonData) == 0 {
+		return errors.New("item doesnt exist")
+	}
+
+	if m == UPDATE {
+		itemIndex := -1
+		for index, item := range jsonData {
+			if item.Id == id {
+				itemIndex = index
+				break
+			}
+		}
+
+		if itemIndex == -1 {
+			return errors.New("item doesnt exist")
+		}
+		jsonData[itemIndex].IsDone = !jsonData[itemIndex].IsDone
+		return WriteJson(string(FILE_NAME), jsonData)
+	} else if m == DELETE {
+		var data []TodoItem = make([]TodoItem, 0)
+		for _, item := range jsonData {
+			if item.Id != id {
+				data = append(data, item)
+			}
+		}
+
+		if len(data) == 0 {
+			return errors.New("item doesnt exist")
+		}
+
+		return WriteJson(string(FILE_NAME), data)
+	}
+
+	return nil
 }
---
+++ todo/utils.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func ReadJson[T any](path string) ([]T, error) {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	defer file.Close()
+	var data []T
+	err = json.NewDecoder(file).Decode(&data)
+
+	if err != nil {
+		if err.Error() == "EOF" {
+			return []T{}, nil
+		}
+		return nil, err
+	}
+
+	return data, err
+}
+
+func WriteJson[T any](path string, data []T) error {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", " ")
+
+	return encoder.Encode(data)
+}
